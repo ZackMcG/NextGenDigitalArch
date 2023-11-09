9e81a915780c2826b0979af2528c16a3988b201c
ZackMcG <zmcgeeh@clemson.edu>
2023-11-09 08:25:43 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+# Import any necessary modules
+
+# Define any necessary functions or classes
+
+# Write the main program logic
+
+if __name__ == '__main__':
+    print("Hello, world!")
+    # Call the main program logic
